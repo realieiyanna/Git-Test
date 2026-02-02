595968c8582cc68183d961b640a726be2d996502
realieiyanna <iwoods2@ut.utm.edu>
2026-02-02 15:56:00 -0600

    Fourth commit 1 to n main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+//function that calls n and adds to make the sum
+int sum(int n) {
+  int total =0;
+  for (int i = 1; i <=  n; i++) {
+    total += i;
+      }
+  return total;
+}
+
 int main () {
 
-  cout<< "Hello World!" <<endl;
+  int n;
+
+  cout<< "Enter a number: ";
+ cin>> n;
+
+ int result = sum(n);
+   cout<< "The sum from 1 to " << n << " is " << result << endl;
+ 
   return 0;
 }
